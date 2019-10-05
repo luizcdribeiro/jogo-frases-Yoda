37c41434cfaedf2a5c14aea9a53915e7a866196c
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2019-10-05 16:19:43 -0300

    modificações de conteudo

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,26 +1,57 @@
-$("#botao-frase").click(fraseALeatoria);
+$("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
-function fraseALeatoria() {
-  $("#spinner").toggle();
-  $.get("http://localhost:3000/frases", trocaFrase)
-  .fail(function(){
-    $("#erro").show();
-    setTimeout(function(){
-      $("#erro").toggle();
-    }, 2000)
-  })
-  .always(function(){
+function fraseAleatoria() {
     $("#spinner").toggle();
-  })
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },1500);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
 
-};
+function trocaFraseAleatoria(data) {
+    var frase = $(".frase");
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
 
-function trocaFrase(data){
-  var frase = $(".frase");
-  var numeroAleatorio = Math.floor(Math.random() * data.length);
+    frase.text(data[numeroAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
 
-  frase.text(data[numeroAleatorio].texto);
 
-  atualizaTamanhoFrase();
-  atualizaTempoInicial(data[numeroAleatorio].tempo);
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    //criacao do objeto JS que guarda a id
+    var dados = {id : fraseId}; 
+
+    //passando objecto como segundo parametro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+    },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
